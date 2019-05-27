cbfa2d1286e3c9c46c9f462c6d5d31a3ba3073b5
Stu Sklinar <stuart.sklinar@alps.education>
2019-05-27 18:03:25 +0100

    Simple service

---
+++ micro-service/Controllers/EnvironmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace micro_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
